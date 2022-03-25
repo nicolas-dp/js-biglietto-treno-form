13c527b3e89022576466c8cc34077f5052fafde8
Nicolas <wellcomenicolas@gmail.com>
2022-03-25 19:07:26 +0100

    Final Project

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,31 +7,66 @@ L'output del prezzo finale va scritto in forma umana (con massimo due decimali, 
  */
 let prezzo_totale;
 let prezzo_sconto;
-
+let prezzo_finito;
 
 // Chiedi all'utente nome e cognome
-let full_name = document.getElementById("nome_cognome").value;
-    document.querySelector('.nome_passeggero').innerHTML = full_name;
-// Chiedi all'utente il numero dei KM da percorrere
-// GENERA NUMERO RANDOM
 
+
+// Chiedi all'utente il numero dei KM da percorrere
+
+
+/* function myFunction() {
+    var form_selector = document.getElementById("form_selector");
+    document.getElementById("offerta").value = form_selector.options[form_selector.selectedIndex].text;
+} */
+
+// GENERA TIcket
 const elementButton = document.querySelector('.btn_genera');
+const sectionTicket = document.querySelector('.section_ticket');
+
+
 
 elementButton.addEventListener('click', function () {
-    
+    // Rimuove la classe con disply none
+    sectionTicket.classList.remove('section_ticket');
 
+    // Cattura il nome completo dell'utente e lo mette alla riga 63 dell'HTML
+    let full_name = document.getElementById("nome_cognome").value;
+    document.querySelector('.nome_passeggero').innerHTML += full_name;
+
+    let option_value = document.getElementById("form_selector").value;
+
+    document.querySelector('.offerta').innerHTML += option_value;
+
+    //Crea un numero randomico per la carrozza
+    let random_number = Math.floor((Math.random() * 10) + 1);
+    document.querySelector(".carrozza").innerHTML += random_number;
+
+     //Crea un numero randomico per la carrozza
+     let random_cp = Math.floor((Math.random() * 9999) + 1);
+     document.querySelector(".cod_cp").innerHTML += random_cp
+
+
+    // Cattura il nemro dei KM e li moltiplica per 0.21€/km 
     let numero_km = document.getElementById("km_da_percorrere").value;
     let prezzo_totale = numero_km * 0.21;
+
+    // Cattura l'età dell'utente e la verifica con la select indicata nel form per poi eseguire lo sconto
     let eta_utente = document.getElementById("form_selector").value;
-    console.log("Il prezzo totale senza sconti è: " + prezzo_totale + " €");
-    if (eta_utente == 1) {
+
+    if (eta_utente == "Minorenne") {
         prezzo_sconto = prezzo_totale - (prezzo_totale * 0.2);
-    } else if (eta_utente == 2) {
+        
+    } else if (eta_utente == "Over 65") {
         prezzo_sconto = prezzo_totale - (prezzo_totale * 0.4);
+        
     } else {
         prezzo_sconto = prezzo_totale;
+        
     }
-    console.log("Il prezzo totale senza sconti è: " + prezzo_sconto + " €");
+
+    prezzo_finito = prezzo_sconto.toFixed(2);
+    document.querySelector(".costo_ticket").innerHTML += prezzo_finito;
     
 })
 
